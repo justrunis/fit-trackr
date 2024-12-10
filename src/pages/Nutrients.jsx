b6b73fb9447e5642b52669463b707b9bf521362b
Justinas Runevičius <justinas.runevicius@ktu.edu>
2024-12-10 16:53:00 +0200

    very scuffed display :)

--- src/pages/Nutrients.jsx
+++ src/pages/Nutrients.jsx
@@ -16,9 +16,11 @@ import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 
 export default function Nutrients() {
-  const [inputValue, setInputValue] = useState("Banana"); // Input controlled state
+  const [inputValue, setInputValue] = useState(""); // Input controlled state
   const [query, setQuery] = useState(""); // State that triggers query
   const [quantity, setQuantity] = useState(1); // Add quantity state
+
+  const [foods, setFoods] = useState([]);
 
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ["natural-nutrients", query],
@@ -31,26 +33,47 @@ export default function Nutrients() {
 
   const food = data?.foods?.[0]; // Get first food item
 
+  const handleAdd = (currentFood) => {
+    setFoods((prevFoods) => [...prevFoods, { ...currentFood, quantity }]);
+  };
+
   return (
     <Container>
-      <TextField
-        label="Search for a food"
-        variant="outlined"
-        fullWidth
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)} // Update input value
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => setQuery(inputValue)} // Set query to trigger search
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          marginBottom: 2,
+          gap: 2,
+        }}
       >
-        Search
-      </Button>
+        <TextField
+          label="Search for a food"
+          variant="outlined"
+          fullWidth
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)} // Update input value
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setQuery(inputValue)} // Set query to trigger search
+        >
+          Search
+        </Button>
+      </Box>
 
       {isLoading && <CircularProgress sx={{ mt: 2 }} />}
 
       {isError && <ErrorIndicator message={error.message} />}
+
+      {!food && !isLoading && !isError && (
+        <Card sx={{ padding: 2 }}>
+          <Typography variant="body1" gutterBottom>
+            No food found
+          </Typography>
+        </Card>
+      )}
 
       {food && (
         <Box
@@ -92,9 +115,52 @@ export default function Nutrients() {
                 <AddIcon />
               </IconButton>
             </Box>
-            <Button variant="contained" color="primary">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => handleAdd({ ...food })}
+            >
               Add
             </Button>
+          </Card>
+        </Box>
+      )}
+
+      {foods.length > 0 && (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Card
+            sx={{
+              width: "80%",
+              maxWidth: 600,
+              padding: 2,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Typography variant="h4" gutterBottom>
+              Added Foods
+            </Typography>
+            {foods.map((food, index) => (
+              <Box key={index} sx={{ display: "flex", alignItems: "center" }}>
+                {console.log(foods)}
+                <img src={food?.photo?.thumb} alt={food?.food_name} />
+                <Typography variant="body1" gutterBottom>
+                  {food?.food_name}
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  {food?.nf_calories * food?.quantity} Total Calories
+                </Typography>
+              </Box>
+            ))}
           </Card>
         </Box>
       )}
